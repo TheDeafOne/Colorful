b60dda118fbfabf564f0b0c9073b61d4a7ca84a3
TheDeafOne <Woodburnkb20@gcc.edu>
2023-11-05 01:14:08 -0400

    base app logic - change secret key

--- application.py
+++ application.py
@@ -1,5 +1,19 @@
 from colorful import create_app
+from colorful.db import db
+import os
+
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+dbfile = os.path.join(scriptdir, "users.sqlite3")
+
+app = create_app()
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = 'correcthorsebatterystaple'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbfile}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+with app.app_context():
+    db.init_app(app)
+    db.create_all() # this is only needed if the database doesn't already exist
 
 if __name__ == "__main__":
-    app = create_app()
     app.run()
