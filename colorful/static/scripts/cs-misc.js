384ac0d4f90d43397f5921cd43bd4bbf561c9420
TheDeafOne <Woodburnkb20@gcc.edu>
2023-11-11 17:14:33 -0500

    working precise geolocation

--- colorful/static/scripts/cs-misc.js
+++ colorful/static/scripts/cs-misc.js
@@ -1,19 +1,48 @@
 
 
 document.addEventListener("DOMContentLoaded", async () => {
-    displayStatusList()
+    displayStatusList();
 
-	// TODO: add an event listener to call search when the button is clicked
-	const button = document.getElementById("setStatus-button")
-	button.addEventListener("click", addStatus)
+    // TODO: add an event listener to call search when the button is clicked
+    const button = document.getElementById("setStatus-button");
+    button.addEventListener("click", addStatus);
 
+    const geoLocateButton = document.getElementById("geolocate-button");
+    geoLocateButton.addEventListener("click", geolocate);
     // const testButton = document.getElementById("test-button")
-	// testButton.addEventListener("click", getStatusList)
+    // testButton.addEventListener("click", getStatusList)
 });
 
-async function addStatus(){
+function geolocate() {
+    console.log('attempting');
+    // Check if geolocation is supported by the browser
+    if ("geolocation" in navigator) {
+        // Prompt user for permission to access their location
+        navigator.geolocation.getCurrentPosition(
+            // Success callback function
+            (position) => {
+                // Get the user's latitude and longitude coordinates
+                const lat = position.coords.latitude;
+                const lng = position.coords.longitude;
+
+                // Do something with the location data, e.g. display on a map
+                console.log(`Latitude: ${lat}, longitude: ${lng}`);
+            },
+            // Error callback function
+            (error) => {
+                // Handle errors, e.g. user denied location sharing permissions
+                console.error("Error getting user location:", error);
+            }
+        );
+    } else {
+        // Geolocation is not supported by the browser
+        console.error("Geolocation is not supported by this browser.");
+    }
+}
+
+async function addStatus() {
     const statusStr = document.getElementById("setStatus-bar").value
-    if(statusStr === ""){
+    if (statusStr === "") {
         return
     }
 
@@ -29,11 +58,10 @@ async function addStatus(){
     })
 
     displayStatusList()
-    // console.log(response)
 }
 
 
-async function getStatusList(){
+async function getStatusList() {
 
     const url = '/api/getStatusList/'
     const response = await fetch(url)
@@ -47,14 +75,14 @@ async function getStatusList(){
     return responseJSON
 }
 
-async function displayStatusList(){
+async function displayStatusList() {
 
     const stati = await getStatusList()
     const statusList = document.getElementById("status-list")
     statusList.innerHTML = ""
 
 
-    for(username in stati){
+    for (username in stati) {
 
         const containerDiv = document.createElement("div")
         containerDiv.className = "border-2 border-black p-4"
