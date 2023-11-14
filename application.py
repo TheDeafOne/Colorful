172e1a11941394b031da343141b680f8ac53f8e5
Jaxaar <jax1664-cs@yahoo.com>
2023-11-13 23:01:23 -0500

    Yay ReloadDB works now
    Auto Reloads the DB if you add:
    RELOAD_DB="True"
    to the .env file

--- application.py
+++ application.py
@@ -32,50 +32,58 @@ def load_user(uid: int) -> database.User:
 with app.app_context():
     database.db.init_app(app)
 
-    # Can't drop tables... So this doesn't do anything
+    if(os.getenv("RELOAD_DB") == "True"):
+        print("Reloading DB...\n") #
+        database.db.drop_all()
+        print("\n ^^^NOT AN ACTUAL ERROR^^^\n\nDatabases Dropped")
+        database.db.create_all()
+        try:
+            # database.db.session.add(database.User(username="UserA", email="a@a.a", password=12345678))
+            users = [
+                database.User(username="UserA", email="a@a.a", password="12345678"),
+                database.User(username="UserB", email="b@b.b", password="12345678"),
+                database.User(username="UserE", email="e@e.e", password="12345678")
+            ]
+            database.db.session.add_all(users)
+            database.db.session.commit()
+            stati = [
+                database.Status(
+                    time="2022-09-20 10:27:21.240752",
+                    text="Hi",
+                    latitude="50",
+                    longitude="50",
+                    color= "#FF0000",
+                    user=users[0].id),
+                database.Status(
+                    time="2022-10-20 10:27:21.240752",
+                    text="Hello",
+                    latitude="60",
+                    longitude="60",
+                    color= "#00FF00",
+                    user=users[1].id),
+                database.Status(
+                    time="2022-11-20 10:27:21.240752",
+                    text="Hello There",
+                    latitude="50",
+                    longitude="80",
+                    color= "#0000FF",
+                    user=users[2].id)
+            ]
+            database.db.session.add_all(stati)
+            database.db.session.commit()
 
-    # if(os.getenv("FILL_DB") == "True"):
-    #     print("Reloading DB...")
-    #     database.db.drop_all()
-    #     database.db.create_all()
-    #     try:
-    #         database.db.session.add(database.User(username="UserA", email="a@a.a", password=12345678))
-    #         users = [
-    #             database.User(username="UserA", email="a@a.a", password=12345678),
-    #             database.User(username="UserB", email="b@b.b", password=12345678),
-    #             database.User(username="UserE", email="e@e.e", password=12345678)
-    #         ]
-    #         database.db.session.add_all(users)
-    #         database.db.session.commit()
-    #         stati = [
-    #             database.Status(
-    #                 time="2022-09-20 10:27:21.240752",
-    #                 text="Hi",
-    #                 latitude=50,
-    #                 longitude=50,
-    #                 color= "#FF0000",
-    #                 user=users[0].id),
-    #             database.Status(
-    #                 time="2022-10-20 10:27:21.240752",
-    #                 text="Hello",
-    #                 latitude=60,
-    #                 longitude=60,
-    #                 color= "#00FF00",
-    #                 user=users[1].id),
-    #             database.Status(
-    #                 time="2022-11-20 10:27:21.240752",
-    #                 text="Hello There",
-    #                 latitude=50,
-    #                 longitude=80,
-    #                 color= "#0000FF",
-    #                 user=users[2].id)
-    #         ]
-    #     except:
-    #         print("Database Re-Population Failed")
+            for i in range(3):
+                users[i].currentStatusID = stati[i].id
+            
+            database.db.session.add_all(users)
+            database.db.session.commit()
 
-    # else:
+            print("Database Loaded")
+        except:
+            print("Database Re-Population Failed")
 
-    database.db.create_all()  # this is only needed if the database doesn't already exist
+    else:
+        database.db.create_all()  # this is only needed if the database doesn't already exist
 
 
 if __name__ == "__main__":
