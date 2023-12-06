213a39829863fec9e9589ccf0a3714c0774f1f0d
TheDeafOne <Woodburnkb20@gcc.edu>
2023-12-06 12:14:36 -0500

    frontend search users

--- colorful/static/scripts/search.js
+++ colorful/static/scripts/search.js
@@ -12,6 +12,31 @@ async function get_users() {
     const query = userSearchInput.value;
     const url = `/api/searchUser/?query=${query}`;
     let response = await fetch(url);
-    let json = await response.json();
-    console.log(json);
+    let users = await response.json();
+
+    // get empty user list container
+    const listElement = document.getElementById("search-results-list");
+    listElement.innerHTML = ""
+
+    // handle no users
+    if (users.length == 0) {
+        const noUsersText = document.createElement("span");
+        noUsersText.innerText = `No user with the name '${query}' found.`
+        listElement.appendChild(noUsersText);
+    }
+
+    // add user card for each user with a username similar to the given query
+    for (user of users) {
+        // make user container div redirect to user profile
+        const containerDiv = document.createElement("div")
+        containerDiv.addEventListener("click", () => window.location.href = `/profile/${user.username}`)
+        containerDiv.className = `bg-white drop-shadow p-4 rounded my-3 border-t-8 border-t-${colorful.getColorName(user.color)}-400 cursor-pointer`
+        const statusSpan = document.createElement("span");
+        statusSpan.innerText = user.username;
+        containerDiv.appendChild(statusSpan);
+        listElement.appendChild(containerDiv);
+    }
+
+    // clear search bar
+    document.getElementById("user-search").value = ""
 }
