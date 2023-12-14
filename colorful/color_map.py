38d3f6a903a588de7f7ed3e90bef9c37ad89a1e7
Kenneth Browder <pandanautinspace@gmail.com>
2023-12-13 19:40:08 -0500

    reimplement emotion mapping

--- colorful/color_map.py
+++ colorful/color_map.py
@@ -1,4 +1,17 @@
-from LeXmo import LeXmo
+import nltk
+nltk.download('punkt')
+import pandas as pd
+from nltk import word_tokenize
+from nltk.stem.snowball import SnowballStemmer
+
+_nrc_f = open('nrc.txt', 'r+')
+_lex_df = pd.read_csv(_nrc_f, names=["word", "emotion", "association"],
+                            sep=r'\t', engine='python')
+_lex_words = _lex_df.pivot(index='word',
+                                   columns='emotion',
+                                   values='association').reset_index()
+_lex_words.drop(_lex_words.index[0])
+_emotions = _lex_words.columns.drop('word')
 
 color_map = {
     'anger': "#FF0000",
@@ -12,10 +25,27 @@ color_map = {
 }
 
 def get_text_emotions(text: str):
-    emo = LeXmo.LeXmo(text)
+    emo = {}
+    document = word_tokenize(text)
+
+    word_count = len(document)
+    rows_list = []
+    for word in document:
+        word = word.lower()
+        emo_score = (_lex_words[_lex_words.word == word])
+        rows_list.append(emo_score)
+    
+    df = pd.concat(rows_list)
+    df.reset_index(drop=True)
+    
+    for emotion in list(_emotions):
+        emo[emotion] = df[emotion].sum() / word_count
+
+
     emo.pop('text', None)
     emo.pop('negative', None)
     emo.pop('positive', None)
+
     return emo
 
 def get_text_emotion(text: str):
