042dbb4832ee1bff189858d7e938020eeae18901
Kenneth Browder <pandanautinspace@gmail.com>
2024-05-21 21:45:12 -0400

    remove lexmo dep

--- colorful/sent2emo.py
+++ colorful/sent2emo.py
@@ -8,22 +8,12 @@ from nltk.corpus import wordnet
 _nrc_f = open('nrc.txt', 'r+')
 _lex_df = pd.read_csv(_nrc_f, names=["word", "emotion", "association"],
                             sep=r'\t', engine='python')
+
 _lex_words = _lex_df.pivot(index='word',
                                    columns='emotion',
                                    values='association').reset_index()
 _lex_words.drop(_lex_words.index[0])
 _emotions = _lex_words.columns.drop('word')
-
-color_map = {
-    'anger': "#FF0000",
-    'anticipation': "#FF8888",
-    'disgust': "#00FF00",
-    'fear': "#000000",
-    'joy': "#FFFF00",
-    'sadness': "#0000FF",
-    'surprise': "#FF8800",
-    'trust': "#333300"
-}
 
 def get_text_emotions(text: str):
     emo = {}
